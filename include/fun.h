7657ed319d6d12518e92cc2f97aec3c8396fae2f
kartofelr <johnmepno@gmail.com>
2024-09-01 21:28:04 +0200

    remove useless files

--- include/fun.h
+++ include/fun.h
@@ -1,6 +1,7 @@
-#ifndef FOO_H
-#define FOO_H
+#ifndef FUN_H
+#define FUN_H
 
-void printgrid(void); // prints the sudoku.txt
+void printgrid(void);   // prints the sudoku.txt
+void naivesearch(void); // sees if any plays are forced
 
-#endif // !FOO_H
+#endif
